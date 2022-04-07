c404554072013aebae8377411227d381b7d0b2ec
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2022-04-07 11:15:08 -0500

    comments rendering, Clear and Streak buttons working properly

--- public/js/habitBoard.js
+++ public/js/habitBoard.js
@@ -1,7 +1,7 @@
 const cardContainer = document.querySelector(".card-container");
 
-const updateStreakNumber = async (id, number) => {
-    await fetch(`/api/habits/streak/${id}/${number}`, {
+const updateStreakNumber = (id, number) => {
+    fetch(`/api/habits/streak/${id}/${number}`, {
         method: "PUT",
         headers: {"Content-type": "application/json; charset=UTF-8"}
     })
@@ -12,12 +12,25 @@ const fetchComments = async id => {
     return await data.json();
 }
 
+const deleteComments = id => {
+    fetch(`/api/comments/${id}`, {
+        method: "DELETE",
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+}
+
 const updateCardClass = (id, color) => {
     const habitCard = document.getElementById(`card-${id}`);
     if (!color) {
         habitCard.attributes.value.value === "positive" ? color = "positive" : color = "negative";
     }
     habitCard.className = `habit-card m-4 rounded shadow ${color}`;
+}
+
+const updateStorageNumber = (id, number) => {
+    const dataObj = JSON.parse(sessionStorage.getItem(`${id}`));
+    dataObj.streakNumber = number;
+    sessionStorage.setItem(`${id}`, JSON.stringify(dataObj));
 }
 
 const modifyNumber = (type, numberElement) => {
@@ -36,34 +49,101 @@ const modifyStreakNumber = (id, type) => {
     return number;
 }
 
-// const renderComments = (cardElement, comments) => {
-//     const html = [""];
-//     comments.forEach(comment => {
-//         html.push(`<div><span>${}</span><span></span></div>`)
-//     })
-// }
+const renderComments = (cardElement, dataObj, habitId) => {
+    const today = (new Date()).toJSON();
+    const html = [`
+    <div class="comments-list">
+        <form class="mx-2 py-1">
+            <label for="commentInput">${today.slice(5, 7) + "/" + today.slice(8, 10)} - </label>
+            <input class="rounded" type="text" name="comment" id="commentInput" placeholder="Add comment here...">
+        </form>`];
+    for (let idx = 0; idx < dataObj.dates.length; idx++) {
+        html.push(`
+            <div class="comment py-1 mx-2">
+                <span class="date-span">${dataObj.dates[idx]} - </span>
+                <p class="ms-1 mb-0">${dataObj.comments[idx]}</p>
+            </div>`)
+    }
+    html.push(`
+        </div>
+        <div class="row-buttons">
+            <button value="${habitId}" class="btn btn-danger m-1">Clear</button>
+            <button value="${habitId}" class="btn btn-success m-1">Streak</button>
+        </div>`);
+    cardElement.innerHTML = html.join("");
+}
+
+const renderCardFront = (cardElement, dataObj, habitId) => {
+    const html = `
+    <div class="row-buttons">
+        <button value="${habitId}" class="btn-primary click-btn m-1">-</button>
+        <button value="${habitId}" class="btn-primary click-btn m-1">+</button>
+    </div>
+    <p id="streak-${habitId}" class="streak-number display-3">${dataObj.streakNumber}</p>
+    <div class="row-buttons">
+        <button value="${habitId}" class="btn btn-danger m-1">Reset</button>
+        <button value="${habitId}" class="btn btn-success m-1">Comments</button>
+    </div>`;
+    cardElement.innerHTML = html;
+}
+
+const parseCommentsData = (data, dates, comments) => {
+    data.forEach(comment => {
+        comments.push(comment.text);
+        dates.push(comment.createdAt.slice(5, 7) + "/" + comment.createdAt.slice(8, 10));
+    })
+    return { dates, comments };
+}
 
 cardContainer.addEventListener("click", async event => {
     if (event.target.attributes && event.target.attributes.value) {
         if (event.target.innerText === "-") {
             const number = modifyStreakNumber(event.target.value, "minus");
             if (number === 0) updateCardClass(event.target.value, "neutral");
+            updateStorageNumber(event.target.value, number);
             updateStreakNumber(event.target.value, number);
         } else if (event.target.innerText === "+") {
             const number = modifyStreakNumber(event.target.value, "plus");
             if (number === 1) updateCardClass(event.target.value, false);
+            updateStorageNumber(event.target.value, number);
             updateStreakNumber(event.target.value, number);
         } else if (event.target.innerText === "Reset") {
             modifyStreakNumber(event.target.value, "reset");
             updateCardClass(event.target.value, "neutral");
+            updateStorageNumber(event.target.value, 0);
             updateStreakNumber(event.target.value, 0);
         } else if (event.target.innerText === "Comments") {
             const habitCardMain = document.querySelector(`#card-${event.target.value} .habit-main`);
-            // const comments = fetchComments(event.target.value);
-            // habitCardMain.innerHTML = "";
+            const data = JSON.parse(sessionStorage.getItem(`${event.target.value}`));
+            renderComments(habitCardMain, data, event.target.value);
+        } else if (event.target.innerText === "Clear") {
+            const data = JSON.parse(sessionStorage.getItem(`${event.target.value}`));
+            data.dates.length = 0;
+            data.comments.length = 0;
+            sessionStorage.setItem(`${event.target.value}`, JSON.stringify(data));
+            renderComments(document.querySelector(`#card-${event.target.value} .habit-main`), data, event.target.value);
+            deleteComments(event.target.value);
+        } else if (event.target.innerText === "Streak") {
+            renderCardFront(
+                document.querySelector(`#card-${event.target.value} .habit-main`),
+                JSON.parse(sessionStorage.getItem(`${event.target.value}`)),
+                event.target.value
+            );
         }
     }
 })
+
+const onPageVisit = async () => {
+    const cardElements = document.querySelectorAll(".habit-card");
+    cardElements.forEach(async card => {
+        const comments = await fetchComments(card.id.slice(5));
+        const storageObj = parseCommentsData(comments, [], []);
+        storageObj.streakNumber = document.getElementById(`streak-${card.id.slice(5)}`).innerText;
+        sessionStorage.setItem(`${card.id.slice(5)}`, JSON.stringify(storageObj));
+    })
+}
+
+onPageVisit();
 
 
 /*
--- routes/api/comments.js
+++ routes/api/comments.js
@@ -2,11 +2,26 @@ const router = require("express").Router();
 const db = require("../../models");
 
 router.get("/:habitId", async (req, res) => {
-    const records = await db.User.findAll({
+    const records = await db.Comments.findAll({
         where: { habitId: req.params.habitId },
         order: [ ["createdAt", "DESC"] ]
     })
     res.json(records);
 });
+
+router.delete("/:habitId", async (req, res) => {
+    await db.Comments.destroy({
+        where: { habitId: req.params.habitId }
+    })
+    res.end();
+});
+
+router.post("/add", async (req, res) => {
+    await db.Comments.create({
+        text: req.body.text,
+        habitId: req.body.habitId 
+    })
+    res.end();
+})
 
 module.exports = router;
