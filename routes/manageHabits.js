0a93c81eadfe6c943730c7235247ce57141a92dd
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2022-04-08 11:24:04 -0500

    loginFailed css updated. manageHabits page only displays habits for logged-in user

--- routes/manageHabits.js
+++ routes/manageHabits.js
@@ -2,16 +2,17 @@ const express = require('express');
 //const bcrypt = require('bcryptjs'); //hash and salt our password
 const router = express.Router();
 const db = require('../models');
-//const auth = require('../auth/index');
+const auth = require('../auth/index');
 
-const findAll = async ()=>{
-    let record = await db.Habits.findAll();
-    return record;
+const findAll = async req => {
+    return await db.Habits.findAll({
+        where: { userId: req.session.passport.user }
+    });
 }
 
-router.get("/manageHabits", async (req, res) => {
+router.get("/manageHabits", auth, async (req, res) => {
     try {
-        const records = await findAll();
+        const records = await findAll(req);
         res.render("manageHabits", { habits: records });
     } catch (err) {
         console.log(err)
@@ -26,7 +27,7 @@ router.delete("/manageHabits/:id", async (req, res) => {
     try {
         let id = req.params.id
         await db.Habits.destroy({ where: { id: id} })
-        let records = await findAll();
+        let records = await findAll(req);
         res.json(records);
        // res.render("manageHabits", { habits: records });
     } catch (err) {
